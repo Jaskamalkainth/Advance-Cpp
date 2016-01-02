6b7894a17a467f7384f7a949155734d02a291073
Jaskamal Kainth <kainthjaskamal@gmail.com>
2016-01-02 09:18:22 +0530

    stuff done in compile time using constexpr

--- constexprrFIB.cpp
+++ constexprrFIB.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 /************ Constexpr ****/
 
+
+/****const expr let's you do stuff in compile time ******/
+
 constexpr size_t fib(size_t n )
 {
 	return n <= 1 ? 1 : fib(n-1) + fib(n-2);  
